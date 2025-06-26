84ddd56f045e4aadf75c1b7fc9fd9872250f574a
AkanIBassey <akaninyeneibassey0@gmail.com>
2025-06-26 16:51:44 +0100

    updated configuration

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,14 @@
 // scripts/app.js
 const firebaseConfig = {
-    apiKey: "AIzaSyBmR6gmbM8RDQupc1PH4040d5UROUDKTmk",
-    authDomain: "happybirthdaypeo.firebaseapp.com",
-    projectId: "happybirthdaypeo",
-    storageBucket: "happybirthdaypeo.firebasestorage.app",
-    messagingSenderId: "1080675049446",
-    appId: "1:1080675049446:web:e07618a7cac6c16c6b4027",
-    measurementId: "G-XGJLKCQTH4"
-  };
+  apiKey: "AIzaSyBmR6gmbM8RDQupc1PH4040d5UROUDKTmk",
+  authDomain: "happybirthdaypeo.firebaseapp.com",
+  databaseURL: "https://happybirthdaypeo-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "happybirthdaypeo",
+  storageBucket: "happybirthdaypeo.firebasestorage.app",
+  messagingSenderId: "1080675049446",
+  appId: "1:1080675049446:web:e07618a7cac6c16c6b4027",
+  measurementId: "G-XGJLKCQTH4"
+};
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
